97eb8e9e0adccc01435dc0097ec8039ae46cfa85
uShire <afgregana@up.edu.ph>
2024-10-08 15:27:26 +0800

    header 90%

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        "light":"#f4f6f3",
+        "light":"#ffffff",
         "dark":"#2b2b2b",
         "primary":"#005E7F",
         "accent":"#ffc139",
+      },
+      backgroundImage:{
+        "banner-desk":"url('/dist/img/cinet-banner-desktop.svg')",
       },
       fontFamily:{
         poppins: ['Poppins', 'sans-serif'],
